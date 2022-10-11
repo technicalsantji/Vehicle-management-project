6450015f9505dfe419b27e2bf716faac0be19d36
Sant Kumar <santkumar11032000@gmail.com>
2022-10-11 21:02:44 +0530

    first commit
    more changes in my project time to time

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+  <footer class="site-footer">
+            <div class="footer-inner bg-white  text-center">
+                <div class="row">
+                    
+                    <div class="col-sm-6">
+                        Developed By Technical santji
+                                  </div>
+                </div>
+            </div>
+        </footer>
